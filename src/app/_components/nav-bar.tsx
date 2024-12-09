624e903e041d8a43860ec24892c8528446019315
Taylor Walenczyk <tawalenczyk@gmail.com>
2024-12-09 15:51:41 -0700

    Add Backpacking Link to NavBar
    Changes the Backpacking text to a link that take users to /backpacking.

--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -1,13 +1,17 @@
 /* Author: Taylor Walenczyk
  * Description: Allows users to view blog posts within a specific category.
  */
+import Link from 'next/link'
 
 export function NavBar() {
     return (
         <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-            <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
+            <Link 
+                href="/backpacking"
+                className="text-center md:text-left text-lg mt-5 md:pl-8"
+            >
                 Backpacking
-            </h4>
+            </Link>
             <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
                 Fastpacking 
             </h4>
