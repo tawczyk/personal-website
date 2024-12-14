00381e12e345983533620b58a8d2295ea2800c30
Taylor Walenczyk <tawalenczyk@gmail.com>
2024-12-14 13:44:56 -0700

    Add /hand
    Adds a new page /hand for climbing related posts.

---
+++ src/app/hand/page.tsx
@@ -0,0 +1,41 @@
+import Container from "@/app/_components/container";
+import { HeroPost } from "@/app/_components/hero-post";
+import { Intro } from "@/app/_components/intro";
+import { MoreStories } from "@/app/_components/more-stories";
+import { getPostsByCategory } from "@/lib/api";
+import { PostCategories } from "@/lib/constants";
+
+export default function ByHand() {
+    const allPosts = getPostsByCategory(PostCategories.get("HAND"));
+
+    if(allPosts !== null && allPosts.length > 0) {
+        const heroPost = allPosts[0];
+
+        const morePosts = allPosts.slice(1);
+
+        return (
+            <main>
+                <Container>
+                    <Intro />
+                    <HeroPost
+                    title={heroPost.title}
+                    coverImage={heroPost.coverImage}
+                    date={heroPost.date}
+                    author={heroPost.author}
+                    slug={heroPost.slug}
+                    excerpt={heroPost.excerpt}
+                    />
+                    {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+                </Container>
+            </main>
+        );
+    } else {
+        return (
+            <main>
+                <Container>
+                    <Intro />
+                </Container>
+            </main>
+        );
+    }
+}
