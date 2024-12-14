c2f4416391943b8c7884cdb8e269f51f7dcec09c
Taylor Walenczyk <tawalenczyk@gmail.com>
2024-12-14 13:33:19 -0700

    Add /bike
    Changes /bikepacking to /bike and updates it to use the latest API.

--- src/app/bike/page.tsx
+++ src/app/bike/page.tsx
@@ -3,9 +3,10 @@ import { HeroPost } from "@/app/_components/hero-post";
 import { Intro } from "@/app/_components/intro";
 import { MoreStories } from "@/app/_components/more-stories";
 import { getPostsByCategory } from "@/lib/api";
+import { PostCategories } from "@/lib/constants";
 
 export default function Backpacking() {
-    const allPosts = getPostsByCategory(2);
+    const allPosts = getPostsByCategory(PostCategories.get("BIKE"));
 
     if(allPosts !== null && allPosts.length > 0) {
         const heroPost = allPosts[0];
