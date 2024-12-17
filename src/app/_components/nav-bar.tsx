3fe37cb05420be200fe5fa65a66c27f06634ddc0
Taylor Walenczyk <tawalenczyk@gmail.com>
2024-12-17 09:51:23 -0700

    Fix "By Foot" Menu Presentation
    The drop down menu for "By Foot" now displays below the "By Foot" button
    and correctly overlaps the subsequent HeroPost. Improvements were made
    to the aesthetic of the drop down menu as well.

--- src/app/_components/nav-bar.tsx
+++ src/app/_components/nav-bar.tsx
@@ -106,12 +106,12 @@ export function NavBar() {
                         leaveFrom="opacity-100 translate-y-0"
                         leaveTo="opacity-0 translate-y-1"
                         >
-                        <Popover.Panel className="z-10">
+                        <Popover.Panel className="absolute top-full z-10 mt-3 overflow-hidden rounded-3xl shadow-lg bg-[#09160E] flex flex-col">
                             {footMenu.map((item) => (
                                 <Link
                                     key={item.name}
                                     href={item.href}
-                                    className="relative flex items-center gap-x-6">{item.name}</Link>
+                                    className="group relative flex items-center gap-x-6 rounded-lg px-4 py-2 text-sm leading-6 hover:bg-gray-800">{item.name}</Link>
                             ))}
                         </Popover.Panel>
                     </Transition>
